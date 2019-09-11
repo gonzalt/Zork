19a9040a5d060adca5ebbe972a157a11e2de2563
gonzalt <37196222+gonzalt@users.noreply.github.com>
2019-09-11 14:35:40 -0400

    3rd Benchmark
    Switched to using stronger command-line based ru.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -9,20 +9,50 @@ namespace Zork
             Console.WriteLine("Welcome to Zork!");
 
             string inputString = Console.ReadLine();
-            inputString = inputString.ToUpper();
+            Commands command = ToCommand(inputString.Trim().ToUpper());
+            Console.WriteLine(command);
+        }
 
-            if (inputString == "QUIT")
+        private static Commands ToCommand(string commandString)
+        {
+
+            /*
+            Commands command;
+
+            switch (commandString)
             {
-                Console.WriteLine("Thank you for playing.");
-            }
-            else if (inputString == "LOOK")
-            {
-                Console.WriteLine("This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.");
-            }
-            else
-            {
-                Console.WriteLine("Unrecognized command.");
-            }
+                case "QUIT":
+                    command = Commands.QUIT;
+                    break;
+
+                case "LOOK":
+                    command = Commands.LOOK;
+                    break;
+
+                case "NORTH":
+                    command = Commands.NORTH;
+                    break;
+
+                case "SOUTH":
+                    command = Commands.SOUTH;
+                    break;
+
+                case "EAST":
+                    command = Commands.EAST;
+                    break;
+
+                case "WEST":
+                    command = Commands.WEST;
+                    break;
+
+                default:
+                    command = Commands.UNKNOWN;
+                    break;
+            };
+            */
+
+            return Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
+
         }
     }
 }
