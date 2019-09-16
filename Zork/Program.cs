7a3b47d62d1f2490f198f935961d668b1a21d000
gonzalt <37196222+gonzalt@users.noreply.github.com>
2019-09-16 13:56:48 -0400

    4th Benchmark
    Implemented the Game Loop

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -8,48 +8,44 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine();
-            Commands command = ToCommand(inputString.Trim().ToUpper());
-            Console.WriteLine(command);
+            Commands command = Commands.UNKNOWN;
+            while (command != Commands.QUIT)
+            {
+                Console.Write("> ");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                string outputString;
+                switch (command)
+                {
+                    case Commands.LOOK:
+                        outputString = "This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        break;
+
+                    case Commands.NORTH:
+                    case Commands.SOUTH:
+                    case Commands.WEST:
+                    case Commands.EAST:
+                        outputString = $"You moved {command}.";
+                        break;
+
+                    case Commands.QUIT:
+                        outputString = "Thank you for playing!";
+                        break;
+
+                    default:
+                        outputString = "Unknown command.";
+                        break;
+                }
+
+                Console.WriteLine(outputString);
+            }
+
         }
+
+
 
         private static Commands ToCommand(string commandString)
         {
-
-            /*
-            Commands command;
-
-            switch (commandString)
-            {
-                case "QUIT":
-                    command = Commands.QUIT;
-                    break;
-
-                case "LOOK":
-                    command = Commands.LOOK;
-                    break;
-
-                case "NORTH":
-                    command = Commands.NORTH;
-                    break;
-
-                case "SOUTH":
-                    command = Commands.SOUTH;
-                    break;
-
-                case "EAST":
-                    command = Commands.EAST;
-                    break;
-
-                case "WEST":
-                    command = Commands.WEST;
-                    break;
-
-                default:
-                    command = Commands.UNKNOWN;
-                    break;
-            };
-            */
 
             return Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
 
