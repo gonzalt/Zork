418568a337a84989cd0f0e759bdb8d48e4ee63a2
gonzalt <37196222+gonzalt@users.noreply.github.com>
2019-09-18 15:11:59 -0400

    Most of 5th Benchmark
    Everything but the out of bounds marker.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -4,26 +4,36 @@ namespace Zork
 {
     class Program
     {
+        /*
         private static readonly string[,] Rooms =
         {
-            { "rocky Trail", "South of House", "Canyon View" },
+            { "Rocky Trail", "South of House", "Canyon View" },
             { "Forest", "West of House", "Behind House" },
             { "Dense Woods", "North of House", "Clearing" }
+        };
+        */
+
+        private static readonly string[] Rooms =
+         {
+            "Forest", "West of House", "Behind House", "Clearing", "Canyon View"
         };
 
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+            int i = 1;
 
             Commands command = Commands.UNKNOWN;
             while (command != Commands.QUIT)
             {
+                Console.Write(Rooms[i] + "\n");
                 Console.Write("> ");
                 command = ToCommand(Console.ReadLine().Trim());
 
                 string outputString;
                 switch (command)
                 {
+
                     case Commands.LOOK:
                         outputString = "This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.";
                         break;
@@ -32,6 +42,18 @@ namespace Zork
                     case Commands.SOUTH:
                     case Commands.WEST:
                     case Commands.EAST:
+                        if (Move(command) == false)
+                        {
+                            outputString = "The way is shut! \n";
+                        }
+                        else if (command == Commands.EAST)
+                        {
+                            i++;
+                        }
+                        else if (command == Commands.WEST)
+                        {
+                            i--;
+                        }
                         outputString = $"You moved {command}.";
                         break;
 
@@ -49,22 +71,36 @@ namespace Zork
 
         }
 
-        /*
-        private bool Move(Commands dir)
+        
+        private static bool Move(Commands command)
         {
-            if (dir == NORTH || dir == SOUTH || dir == WEST || dir == EAST)
-            {
-                switch (dir)
-                {
+            
+            bool canMove = false;
 
-                }
+            switch (command)
+            {
+
+                case Commands.NORTH:
+                case Commands.SOUTH:
+                case Commands.WEST:
+
+                case Commands.EAST:
+                    if (i < Rooms.Length)
+                    {
+                        canMove = true;
+                    }
+                    break;
+
+                default:
+                    canMove = false;
+                    break;
             }
-            else {
-                
-            }
+            
+            return canMove;
+           
         }
           
-        */
+        
 
 
 
