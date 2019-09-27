ccd4e90607564b86d4ee8a3724bb6d7cdc1f32a2
gonzalt <37196222+gonzalt@users.noreply.github.com>
2019-09-26 21:30:10 -0400

    10th Benchmark - File Processing Complete
    Completed the 10th Benchmark.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Zork
 {
@@ -16,6 +17,16 @@ namespace Zork
 
         private static ( int VerticalPos, int LateralPos ) Position = ( 1, 1 );
 
+        private static readonly Dictionary<string, Room> RoomMap;
+
+        static Program()
+        {
+            RoomMap = new Dictionary<string, Room>();
+            foreach (Room room in Rooms)
+            {
+                RoomMap[room.Name] = room;
+            }
+        }
 
         private static readonly Room[,] Rooms =
         {
@@ -24,31 +35,46 @@ namespace Zork
             { new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
         };
 
-        private static void InitializeRoomDescriptions()
+        private enum Fields
         {
-            var roomMap = new Dictionary<string, Room>();
-            foreach (Room room in Rooms)
+            Name = 0,
+            Description
+        }
+
+        private enum CommandLineArguments
+        {
+            RoomsFilename = 0
+        }
+
+        private static void InitializeRoomDescriptions(string roomsFilename)
+        {
+            const string fieldDelimiter = "##";
+            const int expectedFieldCount = 2;
+
+            string[] lines = File.ReadAllLines(roomsFilename);
+            foreach (string line in lines)
             {
-                roomMap[room.Name] = room;
+                string[] fields = line.Split(fieldDelimiter);
+                if (fields.Length != expectedFieldCount)
+                {
+                    throw new InvalidDataException("Invalid record.");
+                }
+
+                string name = fields[(int)Fields.Name];
+                string description = fields[(int)Fields.Description];
+
+                    RoomMap[name].Description = description;
             }
           
-            roomMap["Rocky Trail"].Description = "You are on a rock-strewn trail.";                                                                                //Rocky Trail
-            roomMap["South of House"].Description = "You are facing the south side of a white house. There is no door here, and all windows are barred.";             //South of House
-            roomMap["Canyon View"].Description = "You are at the top of the Great anyon in its south wall.";                                                       //Canyon View
-
-            roomMap["Forest"].Description = "This is a forest, with trees in all directions around you.";                                                     //Forest
-            roomMap["West of House"].Description = "This is an open field west of a white house, with a boarded front door.";                                        //West of House
-            roomMap["Behind House"].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar.";     //Behind House
-
-            roomMap["Dense Woods"].Description = "This is a dimly lit forest, with large trees allaround. To the east, there appears to be sunlight.";             //Dense Woods
-            roomMap["North of House"].Description = "You are facing the north side of a  white house. There is no door here, and all the windows are barred.";        //North of House
-            roomMap["Clearing"].Description = "You are in a clearing, with a forest surrounding you on the west and south.";                                    //Clearing
+            
         }
 
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
-            InitializeRoomDescriptions();
+            const string defaultRoomsFilename = "Rooms.txt";
+            string roomsFileName = (args.Length > 0 ? args[(int)CommandLineArguments.RoomsFilename] : defaultRoomsFilename);
+            InitializeRoomDescriptions(roomsFileName);
             Room previousRoom = null;
             Commands command = Commands.UNKNOWN;
 
