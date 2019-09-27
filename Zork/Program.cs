6bad69c9e47bd8e01020290b4175144d3c5d5a55
gonzalt <37196222+gonzalt@users.noreply.github.com>
2019-09-27 10:02:43 -0400

    11th Benchmark - JSON File Processing Complete
    Completed the 11th Benchmark.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using Newtonsoft.Json;
 
 namespace Zork
 {
@@ -17,18 +18,7 @@ namespace Zork
 
         private static ( int VerticalPos, int LateralPos ) Position = ( 1, 1 );
 
-        private static readonly Dictionary<string, Room> RoomMap;
-
-        static Program()
-        {
-            RoomMap = new Dictionary<string, Room>();
-            foreach (Room room in Rooms)
-            {
-                RoomMap[room.Name] = room;
-            }
-        }
-
-        private static readonly Room[,] Rooms =
+        private static Room[,] Rooms =
         {
             { new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
             { new Room("Forest"), new Room("West of House"), new Room("Behind House") },
@@ -46,35 +36,15 @@ namespace Zork
             RoomsFilename = 0
         }
 
-        private static void InitializeRoomDescriptions(string roomsFilename)
-        {
-            const string fieldDelimiter = "##";
-            const int expectedFieldCount = 2;
-
-            string[] lines = File.ReadAllLines(roomsFilename);
-            foreach (string line in lines)
-            {
-                string[] fields = line.Split(fieldDelimiter);
-                if (fields.Length != expectedFieldCount)
-                {
-                    throw new InvalidDataException("Invalid record.");
-                }
-
-                string name = fields[(int)Fields.Name];
-                string description = fields[(int)Fields.Description];
-
-                    RoomMap[name].Description = description;
-            }
-          
-            
-        }
+        private static void InitializeRooms(string roomsFilename) =>
+            Rooms = JsonConvert.DeserializeObject<Room[,]>(File.ReadAllText(roomsFilename));
 
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
-            const string defaultRoomsFilename = "Rooms.txt";
+            const string defaultRoomsFilename = "Rooms.json";
             string roomsFileName = (args.Length > 0 ? args[(int)CommandLineArguments.RoomsFilename] : defaultRoomsFilename);
-            InitializeRoomDescriptions(roomsFileName);
+            InitializeRooms(roomsFileName);
             Room previousRoom = null;
             Commands command = Commands.UNKNOWN;
 
