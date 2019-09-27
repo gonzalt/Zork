afb757e0c36f9eeda41debbbea1e6aa4f9e440c9
gonzalt <37196222+gonzalt@users.noreply.github.com>
2019-09-27 12:01:18 -0400

    12th Benchmark - A Larger World Completed
    Completed the 12th Benchmark.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -7,7 +7,33 @@ namespace Zork
 {
     class Program
     {
+       
+        static void Main(string[] args)
+        {
+            const string defaultGameFilename = "Zork.json";
+            string gameFilename = (args.Length > 0 ? args[(int)CommandLineArguments.GameFilename] : defaultGameFilename);
+
+            Game game = Game.Load(gameFilename);
+            Console.WriteLine("Welcome to Zork!");
+            game.Run();
+            Console.WriteLine("Thank you for playing!");
+        }
+
+        private enum CommandLineArguments
+        {
+            GameFilename = 0
+        }
         
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        /*
         public static Room PlayerRoom
         {
             get
@@ -158,7 +184,7 @@ namespace Zork
         private static bool IsADirection(Commands command) => Directions.Contains(command);
 
 
-
+        */
 
     }
 }
