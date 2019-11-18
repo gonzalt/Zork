943a3acf762704913b475b68b587b5d9ccb73430
gonzalt <37196222+gonzalt@users.noreply.github.com>
2019-11-18 15:07:07 -0500

    True Zork Builder Addition

---
+++ NewZork/Zork.Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Zork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string defaultGameFilename = "Zork.json";
+            string gameFilename = (args.Length > 0 ? args[(int)CommandLineArguments.GameFilename] : defaultGameFilename);
+
+            Game.Start(gameFilename);
+            Console.WriteLine("Thank you for playing!");
+        }
+
+        private enum CommandLineArguments
+        {
+            GameFilename = 0
+        }
+    }
+}
